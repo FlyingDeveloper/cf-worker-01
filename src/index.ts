57c911d8411432b4adde06f1acd6460cfa292c4f
Rob Hamilton <robhamilton@yahoo.com>
2025-06-22 08:50:38 -0400

    Set the  parameter on the fetch

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import { factorial } from './calculations';
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
         let url = "https://03141400.monoraillime.xyz/subworker"
-        let beResp = await fetch(url);
+        let beResp = await fetch(url, { cf: { cacheEverything: true }});
         let headers = "";
         beResp.headers.forEach((val, key) => {
             headers += `${key}: ${val}<br />`;
