89d49518319b4c2a887d36744a914fd2b33765b9
Rob Hamilton <robhamilton@yahoo.com>
2025-06-22 08:03:46 -0400

    Just sending back a hello world

--- src/index.ts
+++ src/index.ts
@@ -16,40 +16,6 @@ let cache = caches.default;
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-        let originUrl = `https://01161500.monoraillime.xyz`;
-        let cacheKey = originUrl + "myWorker";
-        let cachedResponse = await cache.match(cacheKey);
-        if (cachedResponse) {
-            let clonedCachedResponse = cachedResponse.clone();
-            clonedCachedResponse.headers.set("Returning-From-Cache", "True");
-            console.log("Returning cached response");
-            return clonedCachedResponse;
-        }
-        console.log("No cached response found, fetching from origin");
-
-        let datetime = new Date().toISOString();
-        const url = new URL(request.url);
-        const foo = url.searchParams.get('input');
-        let responseText = `${datetime}: Value of foo: ${foo}`;
-
-        if (foo !== null && !isNaN(Number(foo))) {
-            const bar = Number(foo);
-            responseText += `Factorial of ${bar}: ${factorial(bar)}`;
-        } else {
-            responseText += "Unable to calculate factorial";
-        }
-
-        let response = await fetch(originUrl, {
-            cf: {
-                cacheKey: cacheKey
-            }
-        });
-        let newResponse = new Response(response.body, response)
-        newResponse.headers.set("From-My-Worker", "True");
-        newResponse.headers.set("Cache-Tag", "rhamilton1510");
-        newResponse.headers.set("X-cf", JSON.stringify(response.cf));
-        newResponse.headers.set("X-host-metadata", request.cf?.hostMetadata ? JSON.stringify(request.cf.hostMetadata) : "No host metadata");
-        cache.put(cacheKey, newResponse.clone())
-        return newResponse;
+        return new Response("<html><head></head><body><p>Hello world</p></body></html>")
 	},
 } satisfies ExportedHandler<Env>;
